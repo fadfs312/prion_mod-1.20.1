53609cb1e5c5f9eb8507a9bd50cee66e7635a95b
fadfs312 <130374146+fadfs312@users.noreply.github.com>
2023-11-28 16:28:34 +0100

    Initial commit

---
+++ src/main/java/net/demod/prionmod/PrionMod.java
@@ -0,0 +1,16 @@
+package net.demod.prionmod;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class PrionMod implements ModInitializer {
+	public static final String MOD_ID = "prionmod";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+		LOGGER.info("Hello Fabric world!");
+	}
+}
---
+++ src/main/java/net/demod/prionmod/PrionModClient.java
@@ -0,0 +1,10 @@
+package net.demod.prionmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class PrionModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
